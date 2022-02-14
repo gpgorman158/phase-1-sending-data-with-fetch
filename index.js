be0c1ed86d34314d5d7b0cccaa0823cf522baed4
Gerald Gorman <gpgorman158@gmail.com>
2022-02-14 10:05:52 -0500

    commit tree c6c89942d96997aaaf9322e0f78443defc3d8a14

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData (userName, userEmail){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          dogName: userName,
+          dogBreed: userEmail,
+        }),
+      };
+     
+      return fetch("http://localhost:3000/dogs", configurationObject)
+            .then(response => response.json())
+            .then(data => console.log(data))
+}
+
